3f35002e4d3a6b14a4e0373e936b38482f10f2be
Nikant <nikant20@gmail.com>
2024-12-05 07:41:31 +0530

    Initial commit

---
+++ src/test/java/tech/nikant/testapp/TestappApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nikant.testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
